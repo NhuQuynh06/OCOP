826c02620d7c0263e4dc3f7afdcf169994415803
quynhttn3 <quynhttn3@vng.com.vn>
2023-09-14 23:50:41 +0700

    style

--- source/desktop/js/ui.js
+++ source/desktop/js/ui.js
@@ -1,4 +1,4 @@
-
+// scroll chapter
 var chapterItem = $(".list-chapter .item");
 $(chapterItem).click(function (e) {
   e.preventDefault();
@@ -9,4 +9,27 @@ $(chapterItem).click(function (e) {
     },
     800
   );
+});
+
+// toggle product
+var title = $(".grid-one .title");
+title.click(function (e) {
+  e.preventDefault();
+  var content = $(this).siblings();
+  content.slideToggle();
+});
+
+// nav-tab
+const $navTabLink = $(".nav-tabs .link a");
+const $navTabContent = $(".nav-tabs .content ");
+
+$(".nav-tabs .link a").click(function (e) {
+  // INFO: prevent hash-jumping from anchor element
+  e.preventDefault();
+  const $this = $(e.target);
+  const chosenTab = $this.attr("data-link");
+  $navTabLink.removeClass("active");
+  $this.addClass("active");
+  $navTabContent.find(".tab-pane").removeClass("active");
+  $(".nav-tabs .content ." + chosenTab).addClass("active");
 });
